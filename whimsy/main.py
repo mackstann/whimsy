f613be1a5cf5b4de97bb4251b09ce877306aa72d
Nick Welch <mack@incise.org>
2008-07-09 23:38:11 -0700

    big refactoring of main and signals

--- whimsy/main.py
+++ whimsy/main.py
@@ -49,45 +49,66 @@ startup_shutdown_signal_methods = {
     'wm_shutdown_before': 'shutdown',
 }
 
-methods = hub.register_methods
-
-methods(ewmh.net_supported(),                startup_shutdown_signal_methods)
-methods(ewmh.net_supporting_wm_check(),      startup_shutdown_signal_methods)
-methods(ewmh.net_number_of_desktops(),       startup_shutdown_signal_methods)
-methods(ewmh.net_current_desktop(),          startup_shutdown_signal_methods)
-methods(ewmh.net_desktop_geometry(W*3, H*3), startup_shutdown_signal_methods)
-
-methods(ewmh.net_client_list(), {
+client_list_tracking_signal_methods = {
     'after_manage_window': 'refresh',
     'after_unmanage_window': 'refresh',
     'wm_shutdown_before': 'shutdown',
-})
+}
 
-methods(ewmh.net_desktop_viewport(), {
+viewport_tracking_signal_methods = {
     'wm_manage_after': 'startup',
     'after_viewport_move': 'refresh',
-})
+}
 
-hub.register('wm_manage_after',            discover_existing_windows())
-hub.register('existing_window_discovered', manage_window(), [ if_should_manage_existing_window ])
-hub.register('event', manage_window(),             [ if_(X.MapRequest,     wintype="unmanaged"), if_should_manage_new_window ])
-hub.register('event', client_method('focus'),      [ if_(X.MapRequest,     wintype='client') ])
-hub.register('event', client_method('focus'),      [ if_(X.EnterNotify,    wintype='client'), if_state(~ButtonMask) ])
-hub.register('event', unmanage_window(),           [ if_(X.DestroyNotify,  wintype='client') ])
-hub.register('event', unmanage_window(),           [ if_(X.UnmapNotify,    wintype='client') ])
-hub.register('event', update_client_list_focus(),  [ if_(X.FocusIn,        wintype='client') ])
-hub.register('event', update_client_property(),    [ if_(X.PropertyNotify, wintype='client') ])
-hub.register('event', focus_last_focused(),        [ if_(X.DestroyNotify)    ])
-hub.register('event', install_colormap(),          [ if_(X.ColormapNotify)   ])
-hub.register('event', configure_request_handler(), [ if_(X.ConfigureRequest) ])
-hub.register('event', update_last_button_press(),  [ if_(X.ButtonPress)      ])
+actions = [
+    (startup_shutdown_signal_methods,     ewmh.net_supported()),
+    (startup_shutdown_signal_methods,     ewmh.net_supporting_wm_check()),
+    (startup_shutdown_signal_methods,     ewmh.net_number_of_desktops()),
+    (startup_shutdown_signal_methods,     ewmh.net_current_desktop()),
+    (startup_shutdown_signal_methods,     ewmh.net_desktop_geometry(W*3, H*3)),
+    (viewport_tracking_signal_methods,    ewmh.net_desktop_viewport()),
+    (client_list_tracking_signal_methods, ewmh.net_client_list()),
 
-hub.register('client_init_after', client_method('configure', border_width=0))
-hub.register('client_init_after', client_method('map_normal'))
+    ('wm_manage_after', discover_existing_windows()),
 
-hub.register('event_done', event.smart_replay(),
-    [ if_event_type(X.KeyPress, X.KeyRelease, X.ButtonPress, X.ButtonRelease) ]
-)
+    ('existing_window_discovered', manage_window(),
+     if_should_manage_existing_window),
+
+    ('event', manage_window(),
+     if_(X.MapRequest, wintype="unmanaged"), if_should_manage_new_window),
+
+    ('event', client_method('focus'), if_(X.MapRequest, wintype='client')),
+
+    ('event', client_method('focus'),
+     if_(X.EnterNotify, wintype='client'), if_state(~ButtonMask)),
+
+    ('event', unmanage_window(), if_(X.DestroyNotify, wintype='client')),
+
+    ('event', unmanage_window(), if_(X.UnmapNotify, wintype='client')),
+
+    ('event', update_client_list_focus(), if_(X.FocusIn, wintype='client')),
+
+    ('event', update_client_property(),
+     if_(X.PropertyNotify, wintype='client')),
+
+    ('event', focus_last_focused(), if_(X.DestroyNotify)),
+
+    ('event', install_colormap(), if_(X.ColormapNotify)),
+
+    ('event', configure_request_handler(), if_(X.ConfigureRequest)),
+
+    ('event', update_last_button_press(), if_(X.ButtonPress)),
+
+    ('client_init_after', client_method('configure', border_width=0)),
+
+    ('client_init_after', client_method('map_normal')),
+
+    ('event_done', event.smart_replay(),
+     if_event_type(X.KeyPress, X.KeyRelease, X.ButtonPress, X.ButtonRelease)),
+]
+
+for entry in actions:
+    hub.register(entry[0], entry[1], *entry[2:])
 
 ### SECTION 3 - the playground
 
@@ -101,47 +122,53 @@ class mpd:
         socksend('localhost', 6600, "%s\n" % self.cmd)
 
 actions = [
-    (viewport_absolute_move(  0,   0), [ if_key_press("u",      C+A) ]),
-    (viewport_absolute_move(  W,   0), [ if_key_press("i",      C+A) ]),
-    (viewport_absolute_move(W*2,   0), [ if_key_press("o",      C+A) ]),
-    (viewport_absolute_move(  0,   H), [ if_key_press("j",      C+A) ]),
-    (viewport_absolute_move(  W,   H), [ if_key_press("k",      C+A) ]),
-    (viewport_absolute_move(W*2,   H), [ if_key_press("l",      C+A) ]),
-    (viewport_absolute_move(  0, H*2), [ if_key_press("m",      C+A) ]),
-    (viewport_absolute_move(  W, H*2), [ if_key_press("comma",  C+A) ]),
-    (viewport_absolute_move(W*2, H*2), [ if_key_press("period", C+A) ]),
+    (viewport_absolute_move(  0,   0), if_key_press("u",      C+A)),
+    (viewport_absolute_move(  W,   0), if_key_press("i",      C+A)),
+    (viewport_absolute_move(W*2,   0), if_key_press("o",      C+A)),
+    (viewport_absolute_move(  0,   H), if_key_press("j",      C+A)),
+    (viewport_absolute_move(  W,   H), if_key_press("k",      C+A)),
+    (viewport_absolute_move(W*2,   H), if_key_press("l",      C+A)),
+    (viewport_absolute_move(  0, H*2), if_key_press("m",      C+A)),
+    (viewport_absolute_move(  W, H*2), if_key_press("comma",  C+A)),
+    (viewport_absolute_move(W*2, H*2), if_key_press("period", C+A)),
 
-    (viewport_relative_move(-W,  0),   [ if_key_press("Left",  C) ]),
-    (viewport_relative_move(+W,  0),   [ if_key_press("Right", C) ]),
-    (viewport_relative_move( 0, -H),   [ if_key_press("Up",    C) ]),
-    (viewport_relative_move( 0, +H),   [ if_key_press("Down",  C) ]),
+    (viewport_relative_move(-W,  0), if_key_press("Left",  C)),
+    (viewport_relative_move(+W,  0), if_key_press("Right", C)),
+    (viewport_relative_move( 0, -H), if_key_press("Up",    C)),
+    (viewport_relative_move( 0, +H), if_key_press("Down",  C)),
 
-    (execute("aterm"), [ if_key_press("x", C+A) ]),
-    (execute("aterm"), [ if_root, if_button_press(1, Any), if_multiclick(2) ]),
+    (execute("aterm"), if_key_press("x", C+A)),
+    (execute("aterm"), if_root, if_button_press(1, Any), if_multiclick(2)),
 
-    (execute("sleep 1; xset s activate"), [ if_key_press("s", C+A) ]),
+    (execute("sleep 1; xset s activate"), if_key_press("s", C+A)),
 
-    (mpd("previous"), [ if_key_press("z", M4) ]),
-    (mpd("stop"),     [ if_key_press("x", M4) ]),
-    (mpd("play"),     [ if_key_press("c", M4) ]),
-    (mpd("pause"),    [ if_key_press("v", M4) ]),
-    (mpd("next"),     [ if_key_press("b", M4) ]),
+    (mpd("previous"), if_key_press("z", M4)),
+    (mpd("stop"),     if_key_press("x", M4)),
+    (mpd("play"),     if_key_press("c", M4)),
+    (mpd("pause"),    if_key_press("v", M4)),
+    (mpd("next"),     if_key_press("b", M4)),
 
-    (client_method('focus'),        [ if_client, if_button_press(1, Any, passthru=True) ]),
-    (delete_client(),               [ if_client, if_key_press('w', C+A) ]),
-    (start_move(),                  [ if_button_press(1, A), if_client ]),
-    (start_resize(),                [ if_button_press(3, A), if_client ]),
-    (client_method('stack_bottom'), [ if_button_press(4, A), if_client ]),
-    (client_method('stack_top'),    [ if_button_press(5, A), if_client ]),
+    (client_method('focus'), if_client, if_button_press(1, Any, passthru=True)),
+    (delete_client(),        if_client, if_key_press('w', C+A)),
+    (start_move(),           if_button_press(1, A), if_client),
+    (start_resize(),         if_button_press(3, A), if_client),
+    (client_method('stack_bottom'), if_button_press(4, A), if_client),
+    (client_method('stack_top'),    if_button_press(5, A), if_client),
 
     # maximizations: full screen, left half, right half
-    (client_method('moveresize', x=0,   y=0, width=W,   height=H), [ if_key_press("f", M4) ]),
-    (client_method('moveresize', x=0,   y=0, width=W/2, height=H), [ if_key_press("h", M4) ]),
-    (client_method('moveresize', x=W/2, y=0, width=W/2, height=H), [ if_key_press("l", M4) ]),
+
+    (client_method('moveresize', x=0,   y=0, width=W,   height=H),
+     if_key_press("f", M4)),
+
+    (client_method('moveresize', x=0,   y=0, width=W/2, height=H),
+     if_key_press("h", M4)),
+
+    (client_method('moveresize', x=W/2, y=0, width=W/2, height=H),
+     if_key_press("l", M4)),
 ]
 
-for action, filters in actions:
-    hub.register("event", action, filters)
+for entry in actions:
+    hub.register("event", entry[0], *entry[1:])
 
 ### SECTION 4 - run the damn thing
 
@@ -154,6 +181,26 @@ signal.signal(signal.SIGINT,  lambda signum, frame: ticker.stop())
 signal.signal(signal.SIGPIPE, lambda signum, frame: ticker.stop())
 signal.signal(signal.SIGUSR1, lambda signum, frame: ticker.stop())
 signal.signal(signal.SIGUSR2, lambda signum, frame: ticker.stop())
+
+hub.register("tick", lambda s: signal.alarm(10))
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import logging, logging.handlers
+
+root_logger = logging.getLogger('')
+root_logger.setLevel(logging.DEBUG)
+for handler in root_logger.handlers:
+    root_logger.removeHandler(handler)
+    
+logfilename = os.path.join(os.getcwd(), ".whimsy.log")
+
+root_logger.addHandler(
+    logging.handlers.RotatingFileHandler(logfilename, backupCount=5)
+)
+if os.path.exists(logfilename):
+    root_logger.handlers[0].doRollover()
 
 wm.manage()
 ticker.tick_forever()
--- whimsy/signals.py
+++ whimsy/signals.py
@@ -1,6 +1,6 @@
 # Written by Nick Welch in the years 2005-2008.  Author disclaims copyright.
 
-import logging
+import logging, types
 
 from whimsy import util
 
@@ -17,7 +17,7 @@ class publisher:
     def signal(self, name, **kw):
         import time
         if self.debug:
-            print ('    ' * self.level) + ('%0.3f' % time.time()) + ' <emitting ' + name + '>'
+            logging.debug(('    ' * self.level) + ('%0.3f' % time.time()) + ' <emitting ' + name + '>')
         self.level += 1
         sigdict = self.defaults.copy()
         sigdict.update(kw)
@@ -29,7 +29,7 @@ class publisher:
                     break
             else:
                 if self.debug:
-                    print ('    ' * self.level) + ('%0.3f' % time.time()) + (' <executing %r' % func) + '>'
+                    logging.debug(('    ' * self.level) + ('%0.3f' % time.time()) + (' <executing %r' % func) + '>')
                 self.level += 1
                 ret = func(signal)
                 if ret is not None:
@@ -39,10 +39,13 @@ class publisher:
         self.level -= 1
 
 
-    def register_methods(self, callobj, mapping, filters=[]):
-        for signame, methodname in mapping.items():
-            self.register(signame, getattr(callobj, methodname), filters)
+    def register(self, mapping, callobj, *filters):
+        if isinstance(mapping, types.DictType):
+            for signalname, methodname in mapping.items():
+                self.register(signalname, getattr(callobj, methodname), *filters)
+            return
 
-    def register(self, name, func, filters=[]):
-        self.signals[name] = self.signals.get(name, []) + [[func, filters]]
+        signalname = mapping
+        func = callobj
+        self.signals[signalname] = self.signals.get(signalname, []) + [[func, filters]]
 
