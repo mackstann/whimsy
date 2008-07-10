a15a13af2219d64ec327535b467e3da48554ba56
Nick Welch <mack@incise.org>
2008-07-09 23:37:00 -0700

    remove debug print statement

--- whimsy/actions/event_handling.py
+++ whimsy/actions/event_handling.py
@@ -5,9 +5,8 @@ from whimsy import util, props
 class update_client_property:
     def __call__(self, signal):
         propname = signal.wm.dpy.get_atom_name(signal.ev.atom)
-        if propname not in props.supported_props():
-            return
-        signal.wm.window_to_client(signal.win).update_prop(propname)
+        if propname in props.supported_props():
+            signal.wm.window_to_client(signal.win).update_prop(propname)
 
 # todo: click focus handler & sloppy focus handler
 
@@ -15,8 +14,6 @@ class update_client_property:
 class configure_request_handler:
     def __call__(self, signal):
         client_or_win = signal.wm.window_to_client(signal.win) or signal.win
-        if client_or_win is signal.win:
-            print "configuring raw window: %r" % util.configure_request_changes(signal.ev)
         client_or_win.configure(**util.configure_request_changes(signal.ev))
 
 class install_colormap:
