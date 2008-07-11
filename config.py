48542cac07248438fe85f66000a706ba69032d1a
Nick Welch <mack@incise.org>
2008-07-11 01:14:49 -0700

    commented config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@
 from whimsy.base_config import *
 
 actions = [
+    # these take you to the nine viewports, laid out in a 3x3 grid.
+    # Ctrl+Alt+...
+    #     u i o
+    #     j k l
+    #     m , .
     (viewport_absolute_move(  0,   0), if_key_press("u",      C+A)),
     (viewport_absolute_move(  W,   0), if_key_press("i",      C+A)),
     (viewport_absolute_move(W*2,   0), if_key_press("o",      C+A)),
@@ -13,18 +18,27 @@ actions = [
     (viewport_absolute_move(  W, H*2), if_key_press("comma",  C+A)),
     (viewport_absolute_move(W*2, H*2), if_key_press("period", C+A)),
 
+    # relative movements of the viewport with Ctrl+up, Ctrl+left, etc
     (viewport_relative_move(-W,  0), if_key_press("Left",  C)),
     (viewport_relative_move(+W,  0), if_key_press("Right", C)),
     (viewport_relative_move( 0, -H), if_key_press("Up",    C)),
     (viewport_relative_move( 0, +H), if_key_press("Down",  C)),
 
+    # Ctrl+Alt+x or double click desktop (root window) to open xterm
     (execute("xterm"), if_key_press("x", C+A)),
     (execute("xterm"), if_root, if_button_press(1, Any), if_doubleclick),
 
+    # click on client to focus
     (client_method('focus'), if_client, if_button_press(1, Any, passthru=True)),
+
+    # Ctrl+Alt+w to close window
     (delete_client(), if_client, if_key_press('w', C+A)),
+
+    # Alt+left/right mouse buttons to move/resize interactively
     (start_move(),                  if_button_press(1, A), if_client),
     (start_resize(),                if_button_press(3, A), if_client),
+
+    # Alt+wheelup to lower window, Alt+wheeldown to raise
     (client_method('stack_bottom'), if_button_press(4, A), if_client),
     (client_method('stack_top'),    if_button_press(5, A), if_client),
 ]
