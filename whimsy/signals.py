4543afe0a7671bed65d686edea28847cae508f81
Nick Welch <mack@incise.org>
2008-09-11 04:20:57 -0700

    removing ghetto register mapping thing

--- whimsy/signals.py
+++ whimsy/signals.py
@@ -1,7 +1,5 @@
 # Written by Nick Welch in the years 2005-2008.  Author disclaims copyright.
 
-import types
-
 class publisher(object):
     def __init__(self, **defaults):
         self.signals = {}
@@ -19,14 +17,7 @@ class publisher(object):
             else:
                 ret = func(**kw_dict)
 
-    def register(self, mapping, callobj, *filters):
-        if isinstance(mapping, types.DictType):
-            for name, methodname in mapping.items():
-                self.register_func(name, getattr(callobj, methodname), *filters)
-        else:
-            self.register_func(mapping, callobj, *filters)
-
-    def register_func(self, name, func, *filters):
+    def register(self, name, func, *filters):
         self.signals.setdefault(name, []).append([func, filters])
         # HACK -- filters and actions should be made into the same thing
         for f in filters:
